b6e01c1d6331d99affed48c5bd05cb42a308346b
tanvir_bespoke <tanvir.bespoke@gmail.com>
2022-11-06 13:19:14 +0600

    added

--- Collection/CsvReader.cs
+++ Collection/CsvReader.cs
@@ -10,36 +10,40 @@ namespace Collection
         private string _csvFilePath;
 
         public CsvReader(string csvFilePath)
-       {
-            _csvFilePath = csvFilePath;
-       }
-
-       // get all the country information
-       public Country[] ReadFirstNCountries(int nCountries){
-       
-        // create the array length based on how many country tou wtn to fetched
-        Country[] countries = new Country[nCountries];
-        using (var sr = new StreamReader(_csvFilePath))
         {
-            // omit the first row 
-            // because its just the header in the csv file
-            sr.ReadLine();
-
-            for(int i=0;i<nCountries;i++){
-                string csvLine = sr.ReadLine();
-                countries[i] = ReadCountryFromCsvLine(csvLine);
-            }
+            _csvFilePath = csvFilePath;
         }
-        return countries;
-       }
 
-        public List<Country> ReadAllCountriesList(){
+        // get all the country information
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+
+            // create the array length based on how many country tou wtn to fetched
+            Country[] countries = new Country[nCountries];
+            using (var sr = new StreamReader(_csvFilePath))
+            {
+                // omit the first row 
+                // because its just the header in the csv file
+                sr.ReadLine();
+
+                for (int i = 0; i < nCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+            return countries;
+        }
+
+        public List<Country> ReadAllCountriesList()
+        {
             var countries = new List<Country>();
-            using(var sr = new StreamReader(_csvFilePath))
+            using (var sr = new StreamReader(_csvFilePath))
             {
                 sr.ReadLine(); // omit the first row
                 string row;
-                while((row = sr.ReadLine()) !=null){
+                while ((row = sr.ReadLine()) != null)
+                {
                     countries.Add(ReadCountryFromCsvLineList(row));
                 }
 
@@ -48,16 +52,18 @@ namespace Collection
 
         }
 
-        public Dictionary<string,Country> SaveCountriedwithCodeDict(){
-            var countries = new Dictionary<string,Country>();
+        public Dictionary<string, Country> SaveCountriedwithCodeDict()
+        {
+            var countries = new Dictionary<string, Country>();
             //using stream reader to read 
-            using(StreamReader sr = new StreamReader(_csvFilePath))
+            using (StreamReader sr = new StreamReader(_csvFilePath))
             {
                 sr.ReadLine();
                 string row;
-                while((row = sr.ReadLine())!= null){
-                    var country =  ReadCountryFromCsvLineList(row);
-                    countries.Add(country.Code,country);
+                while ((row = sr.ReadLine()) != null)
+                {
+                    var country = ReadCountryFromCsvLineList(row);
+                    countries.Add(country.Code, country);
                 }
             }
 
@@ -70,14 +76,14 @@ namespace Collection
         {
             var parts = row.Split(',');
             var length = parts.Count();
-            if(length>4)
+            if (length > 4)
             {
-                var name = String.Concat(parts[0],parts[1]);
-                name = name.Replace("\"","").Trim(); /// this Code Replace \" with "" . Replace("\"","") means replace what is inside the "" which is \" and Replace with ""
+                var name = String.Concat(parts[0], parts[1]);
+                name = name.Replace("\"", "").Trim(); /// this Code Replace \" with "" . Replace("\"","") means replace what is inside the "" which is \" and Replace with ""
                 var code = parts[2];
                 var region = parts[3];
                 int population;
-                int.TryParse(parts[4],out population);
+                int.TryParse(parts[4], out population);
                 return new Country(name, code, region, population);
             }
             else
@@ -86,22 +92,67 @@ namespace Collection
                 var code = parts[1];
                 var region = parts[2];
                 int population;
-                int.TryParse(parts[3],out population);
-                return new Country(name,code,region,population);
+                int.TryParse(parts[3], out population);
+                return new Country(name, code, region, population);
             }
-            
-           
+
+
         }
 
 
 
-        public Country ReadCountryFromCsvLine(string csvLine){
-         var parts      = csvLine.Split(',');
-         var name       = parts[0];
-         var code       = parts[1];
-         var region     = parts[2];
-         var population =  int.Parse(parts[3]);
-         return new Country(name, code, region, population);
-       }
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            var parts = csvLine.Split(',');
+            var name = parts[0];
+            var code = parts[1];
+            var region = parts[2];
+            var population = int.Parse(parts[3]);
+            return new Country(name, code, region, population);
+        }
+
+
+
+
+        public Dictionary<string, List<Country>> GetAllCountryBasedONRegion()
+        {
+
+            var countries = new Dictionary<string, List<Country>>();
+            using (var sr = new StreamReader(_csvFilePath))
+            {
+                sr.ReadLine();//omit the first row
+                string row;
+                while ((row = sr.ReadLine()) != null)
+                {
+                    // region is the key
+                    // country list is the value
+                    // to do
+                    // if the key exists then you just add the country in the value list
+                    // if not then first create the dictionary 
+                    // then add both region and valuewith a list
+
+                    // get the country first
+                    var country = ReadCountryFromCsvLineList(row);
+                    // now check if the key exists
+                    if(countries.ContainsKey(country.Region)){
+                        // if already exists
+                        countries[country.Region].Add(country);
+                    }else{
+                        // create the initial list 
+                        var countryList = new List<Country>();
+                        countryList.Add(country);
+                        countries.Add(country.Region,countryList);
+                    }
+
+
+                }
+
+
+            }
+            return countries;
+        }
     }
+
+
+
 }
--- Collection/Program.cs
+++ Collection/Program.cs
@@ -55,4 +55,191 @@ static void Main3(){
     }
 }
 
-Main3();
+
+static void Main4(){
+    var path = getfilePath();
+    var countries_choice = new List<Country>();
+    CsvReader reader = new CsvReader(path);
+    var countries = reader.ReadAllCountriesList();
+    Console.WriteLine("HOW MANY COUNTRY YOU WANT TO SEE ?");
+    bool isint = int.TryParse(Console.ReadLine(),out int input);
+    if(!isint || input <0){
+        Console.WriteLine("YOU MUST USE A POSITIVE NUMBER");
+        return;
+    }
+    // what if the user enter ammount that are 
+    // bigger than the total number of country
+    // so we need to take care of it
+    // we can do it with if else comparison
+    //if(input<=countries.Count){
+    //}else{
+    //}
+    // different approach
+    // we choose which one is min
+    var maxToDisplay = Math.Min(input,countries.Count);
+    for(int i=0;i<maxToDisplay;i++){
+        var country = countries[i];
+        countries_choice.Add(country);
+    }
+    var options = new JsonSerializerOptions();
+    options.WriteIndented = true;
+    var jsonstring = JsonSerializer.Serialize(countries_choice,options);
+    Console.WriteLine(jsonstring);
+   
+
+}
+
+//Main4();
+
+
+static void Pagination(){
+    //1) first get all the countries
+    // 10 %10 = 0
+    // 20 %10 = 0
+    //30 %10 = 0 
+    //increment 10 thats what the mod do  
+    
+    var path = getfilePath();
+    var countries_choice = new List<Country>();
+    CsvReader reader = new CsvReader(path);
+    var countries = reader.ReadAllCountriesList(); // get all the countries
+    Console.WriteLine("ENTER THE BATCH SIZE");
+    var userinput = "10";
+    int.TryParse(userinput,out int max_to_display);
+    for (int i = 0; i < countries.Count; i++)
+    {
+        // fully understand
+        if(i>0 && (i%max_to_display == 0))
+        {
+            // to find a batch we first take number for batch
+            // then we do this i%batch_number
+            // suppose we set 7
+            // then every 7 element i%max_to_display will
+            // be true and i>0 means if it is not then first execution
+            // it will be zero 0/max_to_display = 0 
+            // thats why we did i>0
+            Console.WriteLine("HIT RETURN TO CONTINUE,ANYTHING ELSE TO QUIT");
+            if(Console.ReadLine() != ""){
+                break;
+            }
+
+        }
+
+        var country = countries[i];
+        Console.WriteLine($"NUM : {i+1}  Country Name : {country.Name}, Region : {country.Region}, Code : {country.Code}");
+    }
+    
+
+}
+
+//Pagination();
+
+
+static void ReversePagination(){
+    var path = getfilePath();
+    var countries_choice = new List<Country>();
+    CsvReader reader = new CsvReader(path);
+    var countries = reader.ReadAllCountriesList(); // get all the countries
+    Console.WriteLine("ENTER THE BATCH SIZE");
+    var userinput = "10";
+    int.TryParse(userinput,out int max_to_display);
+    for (int i = countries.Count -1; i >=0; i--)
+    {
+        var index = countries.Count -1 -i; // total number theke prottekbar loop e 1 kore kombe 0 hoyar ag porjonto
+        if(index>0 &&(index%max_to_display == 0)){
+            Console.WriteLine("HIT RETURN TO CONTINUE,ANYTHING ELSE TO QUIT");
+            if(Console.ReadLine() != ""){
+                break;
+            }
+        }
+
+        var country = countries[i];
+        Console.WriteLine($"NUM : {i+1}  Country Name : {country.Name}, Region : {country.Region}, Code : {country.Code}");
+    }
+}
+//ReversePagination();
+
+static void RemoveCommaCountries(){
+
+    var path = getfilePath();
+    CsvReader reader = new CsvReader(path);
+    var countries = reader.ReadAllCountriesList(); // get all the countries
+    Console.WriteLine(countries.Count());
+    var result = countries.RemoveAll(s=>s.Name.Contains(','));
+    Console.WriteLine(countries.Count());
+      
+
+}
+//RemoveCommaCountries();
+
+static void Linq1(){
+    // taking 10 countries with Linq Method
+    var path = getfilePath();
+    CsvReader reader = new CsvReader(path);
+    //var countries = reader.ReadFirstNCountries(10);
+    var countries = reader.ReadAllCountriesList().Take(10);
+    var options = new JsonSerializerOptions();
+    options.WriteIndented = true;
+    var jsonstring = JsonSerializer.Serialize(countries,options);
+    Console.WriteLine(jsonstring);
+    Console.WriteLine("Total Countries: "+ countries.Count().ToString());
+
+    
+}
+
+
+//Linq1();
+
+static void Linq2(){
+    // taking 10 countries with Linq Method
+    var path = getfilePath();
+    CsvReader reader = new CsvReader(path);
+    //var countries = reader.ReadFirstNCountries(10);
+    var countries = reader.ReadAllCountriesList().OrderBy(s=>s.Name).Take(10);
+    var options = new JsonSerializerOptions();
+    options.WriteIndented = true;
+    var jsonstring = JsonSerializer.Serialize(countries,options);
+    Console.WriteLine(jsonstring);
+    Console.WriteLine("Total Countries: "+ countries.Count().ToString());
+
+    
+}
+
+//Linq2();
+
+static void Linqquery(){
+    // taking 10 countries with Linq Method
+    var path = getfilePath();
+    CsvReader reader = new CsvReader(path);
+    //var countries = reader.ReadFirstNCountries(10);
+    //var countries = reader.ReadAllCountriesList().OrderBy(s=>s.Name).Take(10);
+    var countries = (from country in reader.ReadAllCountriesList()
+                    where !country.Name.Contains(',')
+                    orderby country.Name descending
+                    select country).Take(10);
+    var options = new JsonSerializerOptions();
+    options.WriteIndented = true;
+    var jsonstring = JsonSerializer.Serialize(countries,options);
+    Console.WriteLine(jsonstring);
+    Console.WriteLine("Total Countries: "+ countries.Count().ToString());
+
+    
+}
+
+//Linqquery();
+
+static void nestedquery(){
+    // taking 10 countries with Linq Method
+    var path = getfilePath();
+    CsvReader reader = new CsvReader(path);
+    //var countries = reader.ReadFirstNCountries(10);
+    //var countries = reader.ReadAllCountriesList().OrderBy(s=>s.Name).Take(10);
+    var result = reader.GetAllCountryBasedONRegion();
+    var options = new JsonSerializerOptions();
+    options.WriteIndented = true;
+    var jsonstring = JsonSerializer.Serialize(result,options);
+    Console.WriteLine(jsonstring);
+}
+
+nestedquery();
+
